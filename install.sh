587e7b52f53e3393eee0f766239d3d429d023a8c
root <root@f92.vpnservice.company>
2021-09-04 20:14:01 -0400

    tighten up installer

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e
 cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-set -x
+#set -x
 make install
 cp dool.conf /etc/sysconfig/dool.conf
 cp dool-profile.sh /etc/profile.d/dool.sh
 ln -sf $(command -v dool) $(command -v dstat||echo /usr/bin/dstat)
 source /etc/profile.d/dool.sh
-command -v dool
-command -v dstat
-timeout 3 dool ||true
-timeout 3 dool||true
-timeout 3 dstat ||true
-timeout 3 dstat||true
+#command -v dool
+#command -v dstat
+timeout 1 dool ||true
+#timeout 1 dool||true
+timeout 1 dstat ||true
+#timeout 1 dstat||true
