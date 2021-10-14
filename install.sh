bd02e4f11d2b8d85d427ddb6b1fccd4f9eeefe3f
Your Name <you@example.com>
2021-10-13 20:20:54 -0400

    m

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@ set -e
 cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 #set -x
 make install
-cp dool.conf /etc/sysconfig/dool.conf
-cp dool-profile.sh /etc/profile.d/dool.sh
+rsync dool.conf /etc/sysconfig/dool.conf
+rsync dool-profile.sh /etc/profile.d/dool.sh
 ln -sf $(command -v dool) $(command -v dstat||echo /usr/bin/dstat)
-source /etc/profile.d/dool.sh
+#source /etc/profile.d/dool.sh
 #command -v dool
 #command -v dstat
-timeout 1 dool ||true
+#timeout 1 dool ||true
 #timeout 1 dool||true
-timeout 1 dstat ||true
+#timeout 1 dstat ||true
 #timeout 1 dstat||true
