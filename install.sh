06c819a6eb79b8e21588f42b6473c59ad8981915
root <root@f111.vpnservice.company>
2021-10-16 22:58:24 -0400

    tighten up installer

--- install.sh
+++ install.sh
@@ -1,12 +1,31 @@
 #!/usr/bin/env bash
 set -e
 cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+if ! command rsync; then
+	alias rsync=$(command -v cp)
+fi
 #set -x
-command -v make >/dev/null || dnf -y install make
-make install
-rsync dool.conf /etc/sysconfig/dool.conf
-rsync dool-profile.sh /etc/profile.d/dool.sh
-ln -sf $(command -v dool) $(command -v dstat||echo /usr/bin/dstat)
+if ! command -v make; then
+	rsync ./dool /usr/bin/dool && chmod +x /usr/bin/dool
+else
+	make install
+fi
+if ! command -v rsync; then
+  if [[ -d /etc/sysconfig ]]; then
+	rsync dool.conf /etc/sysconfig/dool.conf
+  fi
+  if [[ -d /etc/profile.d ]]; then
+	rsync dool-profile.sh /etc/profile.d/dool.sh
+  fi
+else
+  if [[ -d /etc/sysconfig ]]; then
+	rsync dool.conf /etc/sysconfig/dool.conf
+  fi
+  if [[ -d /etc/profile.d ]]; then
+	rsync dool-profile.sh /etc/profile.d/dool.sh
+  fi
+fi
+ln -sf $(command -v dool) $(command -v dstat || echo /usr/bin/dstat)
 #source /etc/profile.d/dool.sh
 #command -v dool
 #command -v dstat
